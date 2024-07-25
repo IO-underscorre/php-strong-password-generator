5ef4731c8c6574af16671b05d9409383a1868e32
Jacopo Scorretti <jacoposcorretti.99@gmail.com>
2024-07-25 16:35:29 +0200

    build: project files reorganized

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+include_once './functions/functions.php';
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -6,7 +10,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Password Generator</title>
 
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="./syle/style.css">
 </head>
 
 <body>
