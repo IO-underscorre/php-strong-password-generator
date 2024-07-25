2f917eb58e114f08ffff12ef62c06df6101f4002
Jacopo Scorretti <jacoposcorretti.99@gmail.com>
2024-07-26 00:50:56 +0200

    feat(functions.php): functions to generate password completed

--- functions/functions.php
+++ functions/functions.php
@@ -1,1 +1,39 @@
 <?php
+function get_password($password_length, $are_repetitions_allowed, $allowed_chars_string)
+{
+    $allowed_chars = $allowed_chars_string;
+    $password_result = '';
+
+    for ($index = 0; strlen($allowed_chars) && $index < $password_length; $index++) {
+        $random_allowed_chars_index = rand(0, strlen($allowed_chars) - 1);
+
+        $password_result .= substr($allowed_chars, $random_allowed_chars_index, 1);
+
+        if (!$are_repetitions_allowed) {
+            $allowed_chars = substr_replace($allowed_chars, '', $random_allowed_chars_index, 1);
+        }
+    }
+
+    return $password_result;
+}
+
+
+function generate_password($password_length, $are_repetitions_allowed, $are_char_letters_allowed, $are_char_numbers_allowed, $are_char_symbols_allowed)
+{
+    $allowed_chars = '';
+
+    if ($are_char_letters_allowed) {
+        $allowed_chars .= 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $allowed_chars .= strtolower('ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+    }
+
+    if ($are_char_numbers_allowed) {
+        $allowed_chars .= '1234567890';
+    }
+
+    if ($are_char_symbols_allowed) {
+        $allowed_chars .= ' !"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~';
+    }
+
+    return get_password($password_length, $are_repetitions_allowed, $allowed_chars);
+}
